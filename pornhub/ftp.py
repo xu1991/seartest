f9956c110dfda2a3b2331be2612c25b31f66f972
your name <youremail@googl.com>
2019-09-19 16:03:43 +0800

    eqwe

--- pornhub/ftp.py
+++ pornhub/ftp.py
@@ -7,7 +7,7 @@ authorizer = DummyAuthorizer()
 # 将用户名，密码，指定目录，权限 添加到里面
 authorizer.add_user("fan", "root", "/cache", perm="elr")  # adfmw
 # 这个是添加匿名用户,任何人都可以访问，如果去掉的话，需要输入用户名和密码，可以自己尝试
-authorizer.add_anonymous("/cache")
+#authorizer.add_anonymous("/cache")
 
 handler = FTPHandler
 handler.authorizer = authorizer
