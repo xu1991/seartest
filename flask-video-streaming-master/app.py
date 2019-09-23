d1cdd1fc0d7a05e2355d0c47e977c8621124f71b
your name <youremail@googl.com>
2019-09-23 17:19:08 +0800

    fffff

---
+++ flask-video-streaming-master/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, Response
+import cv2
+
+
+class VideoCamera(object):
+    def __init__(self):
+        self.cap = cv2.VideoCapture("rtmp://vod.ahbbtv.com:1935/live/shpd1")
+
+    def __del__(self):
+        self.cap.release()
+
+    def get_frame(self):
+        success, image = self.cap.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
